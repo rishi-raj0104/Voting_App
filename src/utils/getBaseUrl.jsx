ab5ee807778e5b7d438d8be1c003e86b2f141c3b
Rishi Raj <rishi.rishiraj12@gmail.com>
2025-01-22 14:00:51 +0530

    Conclude merge

--- src/utils/getBaseUrl.jsx
+++ src/utils/getBaseUrl.jsx
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:3000"
+    //return "http://localhost:3000"
+    return "https://voting-app-backend-xi.vercel.app/"
 }
 
 export default getBaseUrl;

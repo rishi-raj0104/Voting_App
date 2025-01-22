079e7f9de177bdaa8a2699b0a48f24aa46735d5b
Rishi Raj <rishi.rishiraj12@gmail.com>
2025-01-22 14:04:32 +0530

    first commit

--- src/utils/getBaseUrl.jsx
+++ src/utils/getBaseUrl.jsx
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     //return "http://localhost:3000"
-    return "https://voting-app-backend-xi.vercel.app/"
+    return "https://voting-app-backend-xi.vercel.app"
 }
 
 export default getBaseUrl;

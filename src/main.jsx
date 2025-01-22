55d80ed64c2aaa12ae64e26309814f42ba58c9d9
Rishi Raj <rishi.rishiraj12@gmail.com>
2025-01-22 13:06:15 +0530

    Conclude merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import { RouterProvider } from 'react-router-dom';
-import router from './router.jsx';
+import router from './router';
 import { Provider } from 'react-redux';
-import store from './redux/store.jsx'
+import store from './redux/store'
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <RouterProvider router={router}>
